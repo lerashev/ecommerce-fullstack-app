52b87511386844f313ca3e25a79292f7539fe86d
Lera <lerashevchenko0709@gmail.com>
2022-04-13 21:43:41 -0500

    consts (client)

---
+++ client-2/src/utils/consts.js
@@ -0,0 +1,8 @@
+// will export consts from this file
+
+export const ADMIN_ROUTE = "/admin";
+export const LOGIN_ROUTE = "/login";
+export const REGISTRATION_ROUTE = "/registration";
+export const SHOP_ROUTE = "/";
+export const BASKET_ROUTE = "/basket";
+export const DEVICE_ROUTE = "/device";
